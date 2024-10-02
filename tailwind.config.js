9250754c5d3ca53f207157eff40d1939a1ec2491
jeehwan <tjtojan@naver.com>
2024-10-02 23:21:04 +0900

    FEAT: 글자크기 Global 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      sm: ["14px", "20px"],
+      base: ["18px", "28px"],
+      lg: ["30px", "60px"],
+      xl: ["50px", "100px"],
+    },
   },
   plugins: [],
 };
