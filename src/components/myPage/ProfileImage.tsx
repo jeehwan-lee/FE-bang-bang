c93497ca81844367c1dba585c2112528c8e447f2
jeehwan <tjtojan@naver.com>
2024-10-04 00:35:23 +0900

    FIX: ProfileImage 컴포넌트 props null일 경우 조건부 랜더링 추가

--- src/components/myPage/ProfileImage.tsx
+++ src/components/myPage/ProfileImage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { ReactComponent as ProfileIcon } from "../../icons/profile.svg";
 
 interface ProfileImageProps {
   profileUrl: string;
 }
 function ProfileImage({ profileUrl }: ProfileImageProps) {
-  return <img className="rounded-full w-[160px] h-[160px]" src={profileUrl} />;
+  return;
+  <>
+    {profileUrl ? (
+      <img className="rounded-full w-[160px] h-[160px]" src={profileUrl} />
+    ) : (
+      <ProfileIcon />
+    )}
+  </>;
 }
 
 export default ProfileImage;
